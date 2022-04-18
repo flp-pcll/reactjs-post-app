a956f00c6e10d1452d0ede4aed1790327c01ad37
Felipe Pacelli <fgpacelli1@gmail.com>
2022-04-18 15:02:00 -0300

    Create main app pages and initial routes

--- README.md
+++ README.md
@@ -14,5 +14,13 @@ Como forma de prática, o Posts App é um web app onde o usuário pode:
 - React Router v.6
 - Firebase
 
+## APP
 
+O app será dividido em 3 paginas pricipais:
+
+1) Homepage - renderiza a lista de posts e permite ao usuario filtrar a lista de acordo com a sua preferência. No futuro, será implementada também a funcionalidade de busca de post por palavras;
+
+2) Create - Formulário para criação de posts;
+
+3) Single Post - Exibe um determinado post individualmente, bem como as funcionalidades, como comentário, curtida, etc.
 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Homepage from "./pages/Homepage";
+import CreatePost from "./pages/CreatePost";
+import SinglePost from "./pages/SinglePost";
 
 export default function App() {
-    return <div>App</div>
+    return (
+        <div id="pageWrapper">
+            <header></header>
+            <main>
+                <Routes>
+                    <Route index path="home" element={<Homepage />} />
+                    <Route path="new" element={<CreatePost />}/>
+                    <Route path="singlePost" element={<SinglePost />}/>
+                    <Route path="*" element={<Homepage />} />
+                </Routes>
+            </main>
+            <footer></footer>
+        </div>
+    );
 };
